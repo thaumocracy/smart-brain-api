723b8ade0ad2ae6caa061b1f2cca82688cc7bfdb
Winterstorm <thunpwnz@gmail.com>
2019-02-12 22:06:34 +0400

    port issue fixed

--- server.js
+++ server.js
@@ -49,6 +49,6 @@ app.put('/image',(request,response) => {
     image.handleImage(request,response,db)
 })
 
-app.listen(3000,() => {
-    console.log(`Server listening on port 3000`)
+app.listen(process.event.PORT || 3000,() => {
+    console.log(`Server listening on port ${process.env.PORT}`)
 })
